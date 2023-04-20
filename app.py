bf29096a7d5f395ceecdbfe7ace153206b504053
DAWID-HP\dawid <dawid.bialek@sekonixpoland.com>
2023-04-20 10:34:01 +0200

    Fix problem with form backend

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
-
+import requests
+from flask import *
+from formDTO import *
 
 app = Flask(__name__)
 
 
 @app.route("/index", methods=['GET', 'POST'])
-@app.route("/")
+@app.route("/" , methods=['GET', 'POST'])
 def say_hello():
-    return render_template("index.html")
+    if request.method =="POST":
+        prompt = request.form.get("prompt")
+        print(prompt)
+    return render_template("index.html", prompt=prompt)
 
 
 if __name__ == '__main__':
--- formDTO.py
+++ formDTO.py
@@ -1,4 +1,4 @@
 from wtforms import Form, StringField, validators
 
 class PromptForm(Form):
-    prompt = StringField('Username', [validators.Length(min=1, max=1000)])
+    prompt = StringField('prompt', [validators.Length(min=1, max=1000)])
