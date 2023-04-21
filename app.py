3d787ce6cb68bc60d75a051b83d1f16d2bffdff6
dawid białek <dawid1bialek@gmail.com>
2023-04-21 23:51:46 +0200

    Fix problem with index in first run

--- app.py
+++ app.py
@@ -8,15 +8,19 @@ app = Flask(__name__)
 
 
 
-@app.route("/index", methods=['GET', 'POST'])
-@app.route("/" , methods=['GET', 'POST'])
+@app.route("/index", methods=['GET'])
+@app.route("/" , methods=['GET'])
+def index():
+    return render_template("index.html", prompt="Hi")
+
+@app.route("/index", methods=['POST'])
+@app.route("/" , methods=['POST'])
 def response():
     AIResponse = 'Hi'
     if request.method =="POST":
         userPrompt = request.form.get("prompt")
         AIResponse = sendPromptToGPT(userPrompt)
-    return render_template("index.html", prompt=jsonFormater(AIResponse))
-
+    return render_template("index.html", prompt=AIResponse)
 
 if __name__ == '__main__':
     app.run()
