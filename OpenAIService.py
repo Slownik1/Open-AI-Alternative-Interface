2920b5a325c135613c56bdf7594ae9dfd43b2726
dawid białek <dawid1bialek@gmail.com>
2023-04-22 23:51:25 +0200

    Add davinci model method

--- OpenAIService.py
+++ OpenAIService.py
@@ -1,4 +1,5 @@
 import openai as openai
+import requests
 
 from API_KEY import *
 
@@ -8,7 +9,19 @@ openai.api_key = API_KEY
 
 def sendPromptGPT3Turbo(prompt):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="text-davinci-003",
         messages=[{'role': 'user', 'content': prompt}])
+    return response["choices"][0]["message"]["content"]
+
+def sendPromptDavinci3(prompt):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=150)
+    return response["choices"][0]["text"]
+
+def getAvalibleModel():
+    response =openai.Model.list()
     return response
 
--- app.py
+++ app.py
@@ -16,10 +16,10 @@ def index():
 @app.route("/index", methods=['POST'])
 @app.route("/" , methods=['POST'])
 def response():
-    if request.method =="POST":
-        userPrompt = request.form.get("prompt")
-        AIResponse = sendPromptGPT3Turbo(userPrompt)
-    return render_template("index.html", prompt=jsonFormater(AIResponse))
+
+    userPrompt = request.form.get("prompt")
+    AIResponse = sendPromptDavinci3(userPrompt)
+    return render_template("index.html", prompt=AIResponse)
 
 if __name__ == '__main__':
     app.run()
